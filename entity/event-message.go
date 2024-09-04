eba32b813c5c635df60182d977f55a63b2c213f5
el hombre <ruslan.hoot@icloud.com>
2024-09-04 13:41:42 +0200

    changed event message structure

--- entity/event-message.go
+++ entity/event-message.go
@@ -8,11 +8,10 @@ import (
 
 type EventMessage struct {
 	Sender   *User       `json:"sender,omitempty" bson:"sender"`
-	Type     string      `json:"type,omitempty" bson:"type"`
+	Type     string      `json:"type" bson:"type" validate:"required,min=1"`
 	Time     time.Time   `json:"time,omitempty" bson:"time"`
 	Username string      `json:"username,omitempty" bson:"username"`
-	Status   string      `json:"status,omitempty" bson:"status"`
-	Info     string      `json:"info,omitempty" bson:"info"`
+	Text     string      `json:"text,omitempty" bson:"text"`
 	Payload  interface{} `json:"payload,omitempty" bson:"payload"`
 }
 
